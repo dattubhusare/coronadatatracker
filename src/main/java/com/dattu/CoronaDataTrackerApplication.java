885d21c3ada02b4edd3d49b2489799a859af11eb
goga <dattu40@gmai.com>
2021-04-24 20:04:10 +0530

    create project corona virus data tracker

---
+++ src/main/java/com/dattu/CoronaDataTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.dattu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoronaDataTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoronaDataTrackerApplication.class, args);
+    }
+
+}
