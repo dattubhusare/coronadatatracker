c5c2a4bd9951ab2bd20bbc2d8d77f1bda9552c67
goga <dattu40@gmai.com>
2021-04-26 10:43:50 +0530

    Added Models Classes with Getter and Setter

---
+++ src/main/java/com/dattu/models/LocationStat.java
@@ -0,0 +1,40 @@
+package com.dattu.models;
+
+public class LocationStat {
+    private String state;
+    private  String country;
+    private int latestCases;
+
+    @Override
+    public String toString() {
+        return "LocationStat{" +
+                "state='" + state + '\'' +
+                ", country='" + country + '\'' +
+                ", latestCases='" + latestCases + '\'' +
+                '}';
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLatestCases() {
+        return latestCases;
+    }
+
+    public void setLatestCases(int latestCases) {
+        this.latestCases = latestCases;
+    }
+}
--- src/main/java/com/dattu/service/CoronaDataService.java
+++ src/main/java/com/dattu/service/CoronaDataService.java
@@ -1,5 +1,6 @@
 package com.dattu.service;
 
+import com.dattu.models.LocationStat;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -13,13 +14,18 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class CoronaDataService {
 
+    private List<LocationStat> allStat=new ArrayList<>();
     private String DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     @PostConstruct
     @Scheduled(cron = "* * 1 * * *")
     public void fetchData() throws IOException, InterruptedException {
+        List<LocationStat> newStat=new ArrayList<>();
         HttpClient client=HttpClient.newHttpClient();
         HttpRequest request=HttpRequest.newBuilder()
                 .uri(URI.create(DATA_URL))
@@ -28,9 +34,14 @@ public class CoronaDataService {
         StringReader reader=new StringReader(response.body());
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader().parse(reader);
         for (CSVRecord record : records) {
-            String state = record.get("Province/State");
-            System.out.println(state);
+            LocationStat locationStat=new LocationStat();
+            locationStat.setState(record.get("Province/State"));
+            locationStat.setCountry(record.get("Country/Region"));
+            locationStat.setLatestCases(Integer.parseInt(record.get(record.size()-1)));
+                System.out.println(locationStat);
+                newStat.add(locationStat);
         }
+        this.allStat=newStat;
 
     }
 
