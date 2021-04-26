efb6ce4f7d7a6401b93ca972c33bb16a2e2540ce
goga <dattu40@gmai.com>
2021-04-26 09:51:41 +0530

    Added CSV File Dependency in pom.xml Scheduling Crone Job to load data very day using Spring @Scheduled Annotation

--- src/main/java/com/dattu/CoronaDataTrackerApplication.java
+++ src/main/java/com/dattu/CoronaDataTrackerApplication.java
@@ -2,8 +2,10 @@ package com.dattu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class CoronaDataTrackerApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/dattu/service/CoronaDataService.java
+++ src/main/java/com/dattu/service/CoronaDataService.java
@@ -1,9 +1,14 @@
 package com.dattu.service;
 
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.scheduling.annotation.Schedules;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
 import java.io.IOException;
+import java.io.StringReader;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
@@ -11,15 +16,21 @@ import java.net.http.HttpResponse;
 @Service
 public class CoronaDataService {
 
-    private String DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/04-24-2021.csv";
+    private String DATA_URL="https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchData() throws IOException, InterruptedException {
         HttpClient client=HttpClient.newHttpClient();
         HttpRequest request=HttpRequest.newBuilder()
                 .uri(URI.create(DATA_URL))
                 .build();
         HttpResponse<String> response=client.send(request, HttpResponse.BodyHandlers.ofString());
-        System.out.println(response.body());
+        StringReader reader=new StringReader(response.body());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withHeader().parse(reader);
+        for (CSVRecord record : records) {
+            String state = record.get("Province/State");
+            System.out.println(state);
+        }
 
     }
 
